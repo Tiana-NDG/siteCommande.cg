59ffe14b85503c28438fa4a4de4e7970c129753f
Dahlia Ndebeka <honondebeka@gmail.com>
2019-07-01 15:47:04 +0200

    Le controle au niveau du matricule ou code agent

--- admin/Pages/Agent/AjouterAgent.php
+++ admin/Pages/Agent/AjouterAgent.php
@@ -1,4 +1,5 @@
 <?php
+    require_once("../database.php");
     if(isset($_POST['submit']))
         {
             $matricule = $_POST['matricule'];
@@ -10,33 +11,34 @@
             $ville = $_POST['ville'];
             $adresse = $_POST['adresse'];
 
-            if(!empty($matricule) && !empty($nom) && !empty($prenom) && !empty($genre) && !empty($tel) && !empty($pays) && !empty($ville) && !empty($adresse))
-                {
+            if(empty($matricule) || empty($nom) || empty($prenom) || empty($genre) || empty($tel) || empty($pays) || empty($ville) || empty($adresse) || !preg_match('/^[A-Z0-9_]+$/',$matricule)){
+                echo 'Les champs sont vides ou le format du matricule est incorrecte <a href="javascript:history.back()">Ajouter un nouveau agent</a>';
+            }else {
+                //On verifie si le matricule existe deja dans la base de données
+
+                $req = $bdd->prepare('SELECT CodeAg FROM Agent WHERE CodeAg = ?');
+                $variable = array($matricule);
+                $req->execute($variable);
+                $agent = $req->fetch();
+
+                //si c'est le cas on affiche un message d'erreur
+
+                if($agent){ 
+                    echo 'Le matricule de l\'agent existe déjà dans la base de donnée <a href="javascript:history.back()">Ajouter un nouveau agent</a>';
+                }else{
                     //Enregistrement des données dans la base de donnée
 
-                    require_once("../database.php");
-        
                     $req = $bdd->prepare('INSERT INTO Agent(CodeAg, NomAg, PrenomAg, GenreAg, TelAg, PaysAg, VilleAg, AdresseAg) VALUES(?, ?, ?, ?, ?, ?, ?, ?)');
-
                     $variables = array($matricule, $nom, $prenom, $genre, $tel, $pays, $ville, $adresse);
-    
                     $req->execute($variables);
 
-                    if($req)
-                        {
-                            echo 'Enregistrement éffectué avec succes';
+                    if($req){
+                            echo 'Enregistrement éffectué avec succes, <a href="javascript:history.back()">Ajouter un nouveau agent</a>';
                         }
-                    else
-                        {
-                            echo "Echec d'enregistrement";
+                    else{
+                            echo 'Echec d\'enregistrement, <a href="javascript:history.back()">Ajouter un nouveau agent</a>';
                         }
                 }
-            else
-                {
-                    echo '<p class= "spacer">S.V.P veuillez renseigner tous les champs</p>';
-                }
+            }
+
         }
-
-
-
-    
