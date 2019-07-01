381b3bd8fd1c7754fdb151ca00b574dec9289340
Dahlia Ndebeka <honondebeka@gmail.com>
2019-07-01 15:11:51 +0200

    Controle au niveau de l'ajout du modele

--- admin/Pages/Modele/AjouterModele.php
+++ admin/Pages/Modele/AjouterModele.php
@@ -1,38 +1,36 @@
 <?php
-   
+    require_once("../database.php");
     if(isset($_POST['submit']))
         {
-
             $designation = $_POST['designation'];
             $codemarque = $_POST['marque'];
 
-            if(!empty($designation) && !empty($codemarque) )
-                {
+            if(empty($designation) || empty($codemarque) || !preg_match('/^[a-zA-Z0-9_]+$/',$designation)){
+                echo 'Le champ désignation est vide ou le format de la désignation est incorrecte <a href="javascript:history.back()">Ajouter une nouveau modele</a>';
+            }else{
+                //On verifie si ce surnom existe deja dans la base de données
+
+                $req = $bdd->prepare('SELECT CodeMod FROM Modele_Voiture WHERE DesignationMod = ?');
+                $variable = array($designation);
+                $req->execute($variable);
+                $marque = $req->fetch();
+
+                //si c'est le cas on affiche un message d'erreur
+
+                if($marque){ 
+                    echo 'La désignation du modèle existe déjà dans la base de donnée <a href="javascript:history.back()">Ajouter une nouveau modele</a>';
+                }else {
                     //Enregistrement des données dans la base de donnée
 
-                    require_once("../database.php");
-        
                     $req = $bdd->prepare('INSERT INTO Modele_Voiture(DesignationMod, CodeMarq) VALUES( ?, ?)');
-
                     $variables = array($designation, $codemarque);
-    
                     $req->execute($variables);
 
-                    if($req)
-                        {
-                            echo 'Enregistrement éffectué avec succes';
-                        }
-                    else
-                        {
-                            echo "Echec d'enregistrement";
+                        if($req){
+                            echo 'Enregistrement éffectué avec succes, <a href="javascript:history.back()">Ajouter une nouveau modele</a>';
+                        }else{
+                            echo 'Echec d\'enregistrement, <a href="javascript:history.back()">Ajouter une nouveau modele</a>';
                         }
                 }
-            else
-                {
-                    echo '<p class= "spacer">S.V.P veuillez renseigner tous les champs</p>';
-                }
+            }
         }
-
-
-
-    
